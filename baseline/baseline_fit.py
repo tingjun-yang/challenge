f42ad450f57d262982363a8c62941950899ccf34
q-variance <dorrell@systemsforecasting.com>
2025-11-30 14:33:12 -0500

    Update baseline_fit.py

--- baseline/baseline_fit.py
+++ baseline/baseline_fit.py
@@ -1,24 +1,27 @@
 # baseline_fit.py
+# comment out lines to read files in 3 parts or 1 part
 import pandas as pd
 import numpy as np
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
 # load the parquet files from data_loader.py
-df = pd.concat([
-    pd.read_parquet("dataset_part1.parquet"),
-    pd.read_parquet("dataset_part2.parquet"),
-    pd.read_parquet("dataset_part3.parquet")
-])
+df = pd.concat([pd.read_parquet("dataset_part1.parquet"),pd.read_parquet("dataset_part2.parquet"),pd.read_parquet("dataset_part3.parquet")])
 
+# Select S&P 500, T=5
+# data = df[(df["ticker"] == "^GSPC") & (df["T"] == 5)].copy()
+# data = df[(df["ticker"] == "^GSPC") ].copy()
 data = df.copy()
 data["var"] = data.sigma**2
 
 #print(f"S&P 500 T=5: {len(data)} windows")
+print(f"{len(data)} windows")
+# print(f"mean z = {data["z"].mean()}")  # mean of z is zero
 print(f"z has NaNs: {data['z'].isna().sum()}")  # → 0
 
+
 zmax = 0.6
-delz = 0.025
+delz = 0.025*2
 nbins = int(2*zmax/delz + 1)
 #bins = np.linspace(-0.5, 0.5, 41)         # fixed bins
 bins = np.linspace(-zmax, zmax, nbins)         # fixed bins
@@ -34,7 +37,7 @@ binned = (data.assign(z_bin=pd.cut(data.z, bins=bins, include_lowest=True))
 def qvar(z, s0, zoff):    # define q-variance function, parameter is minimal volatility s0
     return (s0**2 + (z - zoff)**2 / 2)
 
-# curve_fit returns a value popt and a covariance pcov, the _ means we ignore the pcov
+# curve_fit returns a value popt and a covariance pcov
 popt, _ = curve_fit(qvar, binned.z_mid, binned["var"], p0=[0.02, 0])
 
 fitted = qvar(binned.z_mid, popt[0], popt[1])  # cols are z_bin, which is a range like (-0.601, -0.55], and qvar
@@ -43,10 +46,15 @@ r2 = 1 - np.sum((binned["var"] - fitted)**2) / np.sum((binned["var"] - binned["v
 print(f"σ₀ = {popt[0]:.4f}  zoff = {popt[1]:.4f}  R² = {r2:.4f}")
 
 # plot of all stocks
+markfac = 1  # default is 1, can increase to 3 if less data points
 plt.figure(figsize=(9,7))
-plt.scatter(data.z, data['var'], c='steelblue', alpha=0.1, s=1, edgecolor='none')
+plt.scatter(data.z, data['var'], c='steelblue', alpha=markfac*0.1, s=markfac*1, edgecolor='none')
+numeric_array = (1 - data["T"]/130)
+string_array = [str(x) for x in numeric_array]
+#plt.scatter(data.z, data['var'], c='steelblue', alpha=numeric_array, s=1, edgecolor='none')
+#plt.scatter(data.z, data['var'], c=string_array, alpha=0.1, s=1, edgecolor='none')
 plt.plot(binned.z_mid, binned['var'], 'b-', lw=3)     # label='binned'
-plt.plot(binned.z_mid, fitted, 'red', lw=4, label=f'σ₀ = {popt[0]:.3f}, zoff = {popt[1]:.3f}, R² = {r2:.3f}')
+plt.plot(binned.z_mid, fitted, 'red', lw=3, label=f'σ₀ = {popt[0]:.3f}, zoff = {popt[1]:.3f}, R² = {r2:.3f}')
 
 plt.xlabel('z (scaled log return)', fontsize=12)
 plt.ylabel('Annualised variance', fontsize=12)
@@ -59,6 +67,3 @@ plt.legend(fontsize=12)
 plt.grid(alpha=0.3)
 plt.tight_layout()
 plt.show()
-
-
-
